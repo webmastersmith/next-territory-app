3989e6e2fa5fd146c78195a23050d7ba05ae9f8c
Bryon Smith <bryon@smithauto.us>
2022-03-02 21:24:34 -0600

    add card body info

--- components/cards/card/cardBody/cardBody.tsx
+++ components/cards/card/cardBody/cardBody.tsx
@@ -5,6 +5,7 @@ import {
   DeedSvg,
   MailingAddressSvg,
   PhysicalAddressSvg,
+  ExemptionSvg,
 } from 'icons'
 import styles from './cardBody.module.scss'
 
@@ -14,12 +15,59 @@ interface Props {
 //NextPage<Props>
 
 export const CardBody: NextPage<Props> = ({ owner }) => {
+  const {
+    landId,
+    name,
+    deed,
+    nameDeedSame,
+    physicalAddress,
+    physicalCity,
+    physicalState,
+    physicalZip,
+    mailingAddress,
+    mailingCity,
+    mailingState,
+    mailingZip,
+  } = owner
   return (
     <div className={styles.cardBodyWrapper}>
-      <PersonSvg />
-      <DeedSvg />
-      <PhysicalAddressSvg />
-      <MailingAddressSvg />
+      <p>{landId}</p>
+      <div className={styles.svgGrid}>
+        <PersonSvg />
+        <div className={styles.info}>
+          <p className={styles.label}>Owner Name</p>
+          <p>{name}</p>
+        </div>
+        <p>{''}</p>
+
+        <DeedSvg />
+        <div className={styles.info}>
+          <p className={styles.label}>Deed</p>
+          <p>{deed}</p>
+        </div>
+        <p>{''}</p>
+
+        <PhysicalAddressSvg />
+        <div className={styles.info}>
+          <p className={styles.label}>Physical Address</p>
+          <p>{`${physicalAddress} ${physicalCity} ${physicalState} ${physicalZip}`}</p>
+        </div>
+        <p>{''}</p>
+
+        <MailingAddressSvg />
+        <div className={styles.info}>
+          <p className={styles.label}>Mailing Address</p>
+          <p>{`${mailingAddress} ${mailingCity} ${mailingState} ${mailingZip}`}</p>
+        </div>
+        <p>{''}</p>
+
+        <ExemptionSvg />
+        <div className={styles.info}>
+          <p className={styles.label}>Tax Exemptions</p>
+          <p>{name}</p>
+        </div>
+        <p>{''}</p>
+      </div>
     </div>
   )
 }
