a855d29dbdd4a775e0c131478760ecd7dc98ba49
Bryon Smith <bryon@smithauto.us>
2022-03-12 17:46:00 -0600

    custom add images

--- components/layout/hero/hero.tsx
+++ components/layout/hero/hero.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from 'next'
-// import Image from 'next/image'
-import Image from 'components/image'
+import Image from 'next/image'
 import styles from './hero.module.scss'
 import { HomeWorkIcon } from 'icons'
 import animation from 'styles/float.module.css'
@@ -26,7 +25,7 @@ export const Hero: NextPage = () => {
         />
       </div>
 
-      <div className={`${styles.heroImgWrapper} ${animation.float}`}>
+      {/* <div className={`${styles.heroImgWrapper} ${animation.float}`}>
         <Image
           src={headerCard}
           alt="territory card"
@@ -35,7 +34,32 @@ export const Hero: NextPage = () => {
           priority
           className={styles.heroImg}
         />
-      </div>
+      </div> */}
+
+      <picture className={styles.heroImg}>
+        <source
+          type="image/avif"
+          srcSet="/hero/heroCard.png-300w459h.avif 300w, /hero/heroCard.png-600w918h.avif 600w, /hero/heroCard.png-900w1377h.avif 900w"
+        />
+
+        <source
+          type="image/webp"
+          srcSet="/hero/heroCard.png-300w459h.webp 300w, /hero/heroCard.png-600w918h.webp 600w, /hero/heroCard.png-900w1377h.webp 900w"
+        />
+
+        <source
+          type="image/jpg"
+          srcSet="/hero/heroCard.png-300w459h.jpg 300w, /hero/heroCard.png-600w918h.jpg 600w, /hero/heroCard.png-900w1377h.jpg 900w"
+        />
+
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img
+          src="/hero/_heroCard.png"
+          alt="territory card"
+          width={300}
+          height={459}
+        />
+      </picture>
     </header>
   )
 }
--- next.config.js
+++ next.config.js
@@ -1,15 +1,5 @@
 /** @type {import('next').NextConfig} */
-const withOptimizedImages = require('next-optimized-images')
 
-const nextConfig = {
-  images: {
-    loader: 'custom',
-  },
-}
-
-module.exports = withOptimizedImages({
-  /* config for next-optimized-images */
-
-  // your config for other plugins or the general next.js here...
+module.exports = {
   reactStrictMode: true,
-})
+}
