b5c4aa30fe0dc7e9b4c6eb040c6736a8099816ee
Bryon Smith <bryon@smithauto.us>
2022-03-12 21:39:41 -0600

    add custom images

--- components/layout/hero/hero.tsx
+++ components/layout/hero/hero.tsx
@@ -13,7 +13,7 @@ export const Hero: NextPage = () => {
 
       <h1 className={styles.title}>Lufkin Central Address Helper</h1>
 
-      <div className={styles.bgImg}>
+      {/* <div className={styles.bgImg}>
         <Image
           src={pen}
           alt="letter writing"
@@ -23,20 +23,72 @@ export const Hero: NextPage = () => {
           priority
           className={styles.img}
         />
-      </div>
-
-      {/* <div className={`${styles.heroImgWrapper} ${animation.float}`}>
-        <Image
-          src={headerCard}
-          alt="territory card"
-          width={300}
-          height={459}
-          priority
-          className={styles.heroImg}
-        />
       </div> */}
 
-      <picture className={styles.heroImg}>
+      <picture className={styles.picture}>
+        <source
+          media="(max-width: 375px)"
+          type="image/avif"
+          srcSet="/pen/pen.jpg-375w281h.avif 375w, /pen/pen.jpg-750w562h.avif 750w, /pen/pen.jpg-1125w843h.avif 1125w"
+        />
+
+        <source
+          media="(max-width: 375px)"
+          type="image/webp"
+          srcSet="/pen/pen.jpg-375w281h.webp 375w, /pen/pen.jpg-750w562h.webp 750w, /pen/pen.jpg-1125w843h.webp 1125w"
+        />
+
+        <source
+          media="(max-width: 375px)"
+          type="image/jpg"
+          srcSet="/pen/pen.jpg-375w281h.jpg 375w, /pen/pen.jpg-750w562h.jpg 750w, /pen/pen.jpg-1125w843h.jpg 1125w"
+        />
+
+        <source
+          media="(max-width: 768px)"
+          type="image/avif"
+          srcSet="/pen/pen.jpg-768w576h.avif 768w, /pen/pen.jpg-1536w1152h.avif 1536w, /pen/pen.jpg-2304w1728h.avif 2304w"
+        />
+
+        <source
+          media="(max-width: 768px)"
+          type="image/webp"
+          srcSet="/pen/pen.jpg-768w576h.webp 768w, /pen/pen.jpg-1536w1152h.webp 1536w, /pen/pen.jpg-2304w1728h.webp 2304w"
+        />
+
+        <source
+          media="(max-width: 768px)"
+          type="image/jpg"
+          srcSet="/pen/pen.jpg-768w576h.jpg 768w, /pen/pen.jpg-1536w1152h.jpg 1536w, /pen/pen.jpg-2304w1728h.jpg 2304w"
+        />
+
+        <source
+          media="(max-width: 1440px)"
+          type="image/avif"
+          srcSet="/pen/pen.jpg-1440w1080h.avif 1440w, /pen/pen.jpg-2880w2160h.avif 2880w, /pen/pen.jpg-4320w3240h.avif 4320w"
+        />
+
+        <source
+          media="(max-width: 1440px)"
+          type="image/webp"
+          srcSet="/pen/pen.jpg-1440w1080h.webp 1440w, /pen/pen.jpg-2880w2160h.webp 2880w, /pen/pen.jpg-4320w3240h.webp 4320w"
+        />
+
+        <source
+          media="(max-width: 1440px)"
+          type="image/jpg"
+          srcSet="/pen/pen.jpg-1440w1080h.jpg 1440w, /pen/pen.jpg-2880w2160h.jpg 2880w, /pen/pen.jpg-4320w3240h.jpg 4320w"
+        />
+
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img
+          src="/pen/pen.jpg"
+          alt="background image of old english pen"
+          className={styles.penImg}
+        />
+      </picture>
+
+      <picture>
         <source
           type="image/avif"
           srcSet="/hero/heroCard.png-300w459h.avif 300w, /hero/heroCard.png-600w918h.avif 600w, /hero/heroCard.png-900w1377h.avif 900w"
@@ -54,8 +106,9 @@ export const Hero: NextPage = () => {
 
         {/* eslint-disable-next-line @next/next/no-img-element */}
         <img
-          src="/hero/_heroCard.png"
+          src="/hero/heroCard.png-300w459h.jpg"
           alt="territory card"
+          className={`${styles.heroImg} ${animation.float}`}
           width={300}
           height={459}
         />
