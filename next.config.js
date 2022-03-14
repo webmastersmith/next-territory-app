e2d76329e41dead5082dfad18444e8abbab9a715
Bryon Smith <bryon@smithauto.us>
2022-03-14 10:51:46 -0500

    remove public folder to avoid duplicates

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
   reactStrictMode: true,
-  basePath: isProd
-    ? 'https://webmastersmith.github.io/next-territory-app/'
-    : '',
+  basePath: isProd ? '/next-territory-app' : '/',
+  // must have trailing slash
+  assetPrefix: isProd ? '/next-territory-app/' : '',
 }
