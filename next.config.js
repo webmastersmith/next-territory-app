e92640d1db8020d316044395d402d03e1c1ab8d1
Bryon Smith <bryon@smithauto.us>
2022-03-14 10:09:03 -0500

    add basepath to next.config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 
+const isProd = process.env.NODE_ENV === 'production'
+
 module.exports = {
   reactStrictMode: true,
+  basePath: isProd
+    ? 'https://webmastersmith.github.io/next-territory-app/'
+    : '',
 }
